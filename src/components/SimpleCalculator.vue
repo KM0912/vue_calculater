fdea533248430341392043013b41110a743ce491
KM0912 <61630275+KM0912@users.noreply.github.com>
2021-05-11 08:16:06 +0900

    HTML CSSの作成

--- src/components/SimpleCalculator.vue
+++ src/components/SimpleCalculator.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>SimpleCalculator</div>
+  <div >
+    <div class="title">SimpleCalculator</div>
+    <div class="calculator">
+      <div class="display"></div>
+      <div class="btn">7</div>
+      <div class="btn">8</div>
+      <div class="btn">9</div>
+      <div class="operator">C</div>
+      <div class="btn">4</div>
+      <div class="btn">5</div>
+      <div class="btn">6</div>
+      <div class="operator">-</div>
+      <div class="btn">1</div>
+      <div class="btn">2</div>
+      <div class="btn">3</div>
+      <div class="operator">+</div>
+      <div class="btn zero">0</div>
+      <div class="operator">=</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,18 +32,31 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.title {
+  margin-bottom: 8px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.calculator {
+  margin: 0 auto;
+  width: 400px;
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.display {
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: white;
 }
-a {
-  color: #42b983;
+.btn {
+  background-color: #eee;
+  border: 1px solid #999;
+}
+.zero {
+  grid-column: 1 / 4;
+}
+.operator {
+  background-color:steelblue;
+  border: 1px solid #999;
 }
 </style>
